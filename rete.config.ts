72e35e230064e6dc13cee1842885faea2aedea1b
Ni55aN <huccah@gmail.com>
2022-11-28 23:54:30 +0200

    add deps to rollup globals

--- rete.config.ts
+++ rete.config.ts
@@ -2,5 +2,9 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
     input: 'src/index.ts',
-    name: 'ScopesPlugin'
+    name: 'ScopesPlugin',
+    globals: {
+        'rete': 'Rete',
+        'rete-area-plugin': 'ReteAreaPlugin'
+    }
 }
