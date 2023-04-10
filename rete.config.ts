65756aae5f0d7a5a223bb7730de8cb1d2a62efe4
Ni55aN <huccah@gmail.com>
2023-04-10 21:01:16 +0300

    fix umd namespace

--- rete.config.ts
+++ rete.config.ts
@@ -3,7 +3,7 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
-  name: 'ScopesPlugin',
+  name: 'ReteScopesPlugin',
   globals: {
     'rete': 'Rete',
     'rete-area-plugin': 'ReteAreaPlugin'
